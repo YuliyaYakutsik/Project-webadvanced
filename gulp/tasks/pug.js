97a3707eb35b9c0aa61b76f42569b90d55ac24e4
Yuliya Yakutsik <juliayakutik@gmail.com>
2017-04-21 01:36:43 +0200

    pages

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -3,7 +3,9 @@
 module.exports = function() {
   $.gulp.task('pug', function() {
     return $.gulp.src('./source/template/pages/*.pug')
-      .pipe($.gp.pug({ pretty: true }))
+      .pipe($.gp.pug({ 
+        pretty: true,
+      }))
       .on('error', $.gp.notify.onError(function(error) {
         return {
           title: 'Pug',
